dfcad79bf78a8d6e5026408a495f6b96d2d0142b
yashPat98 <prime.ash75@gmail.com>
2021-07-20 21:51:59 +0530

    Win32 Assignments Upload

---
+++ 01-Win32/02-Events/Window.cpp
@@ -0,0 +1,92 @@
+//headers
+#include <windows.h>
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarations
+
+//entry point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    //variable declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("Events");
+
+    //code
+    //initialize WNDCLASSEX
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    //register above class
+    RegisterClassEx(&wndclass);
+
+    //create the window in memory
+    hwnd = CreateWindow(szAppName,
+        szAppName,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL, 
+        NULL,
+        hInstance,
+        NULL);
+    
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    //message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    //code
+    switch(iMsg)
+    {
+        case WM_CREATE:
+            MessageBox(hwnd, TEXT("WM_CREATE message is arrived !"), TEXT("Message"), MB_OK);
+            break;
+
+        case WM_LBUTTONDOWN:
+            MessageBox(hwnd, TEXT("left mouse button is clicked !"), TEXT("Message"), MB_OK);
+            break;
+
+        case WM_RBUTTONDOWN:
+            MessageBox(hwnd, TEXT("right mouse button is clicked !"), TEXT("Message"), MB_OK);
+            break;
+
+        case WM_KEYDOWN:
+            MessageBox(hwnd, TEXT("a key is pressed !"), TEXT("Message"), MB_OK);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            break;
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
---
+++ 01-Win32/04-HelloWorld/Window.cpp
@@ -0,0 +1,91 @@
+//headers
+#include <windows.h>
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarations
+
+//entry point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    //variable declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("Hello World");
+
+    //code
+    //initialize WNDCLASSEX
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    //register above class
+    RegisterClassEx(&wndclass);
+
+    //create the window in memory
+    hwnd = CreateWindow(szAppName,
+        szAppName,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL, 
+        NULL,
+        hInstance,
+        NULL);
+    
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    //message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    //variable declarations
+    HDC hdc;
+    RECT rc;
+    PAINTSTRUCT ps;
+    TCHAR str[] = TEXT("Hello World !!!");
+
+    //code
+    switch(iMsg)
+    {
+        case WM_PAINT:
+            GetClientRect(hwnd, &rc);
+            hdc = BeginPaint(hwnd, &ps);
+            SetBkColor(hdc, RGB(0, 0, 0));
+            SetTextColor(hdc, RGB(0, 255, 0));
+            DrawText(hdc, str, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            EndPaint(hwnd, &ps);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        
+        default:
+            break;
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
