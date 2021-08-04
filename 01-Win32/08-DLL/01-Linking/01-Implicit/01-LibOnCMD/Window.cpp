cc79c8911b1721ef674017e9d08a2f8bbb78df82
yashPat98 <prime.ash75@gmail.com>
2021-08-04 22:46:59 +0530

    WinDev2021 uploda

---
+++ 01-Win32/08-DLL/01-Linking/01-Implicit/01-LibOnCMD/Window.cpp
@@ -0,0 +1,87 @@
+//headers
+#include <windows.h>
+#include "Window.h"
+#include "MyMath.h"
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//entry point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    //variable declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("Implicit DLL");
+
+    //code
+    //initialize WNDCLASSEX
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+    //register above class
+    RegisterClassEx(&wndclass);
+
+    //create the window in memory
+    hwnd = CreateWindow(szAppName,
+        szAppName,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL, 
+        NULL,
+        hInstance,
+        NULL);
+    
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    //message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    //variable declaration
+    int num1, num2;
+    TCHAR str[255];
+
+    //code
+    switch(iMsg)
+    {
+        case WM_CREATE:
+            num1 = 10;
+            num2 = MakeSquare(num1);
+            wsprintf(str, TEXT("Square of %d is %d."), num1, num2);
+            MessageBox(hwnd, str, TEXT("square"), MB_OK);    
+            break;
+        
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        
+        default:
+            break;
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
