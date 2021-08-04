57260d93011b3f2ac6373b0aa7b4c7941cdf1987
yashPat98 <prime.ash75@gmail.com>
2021-08-05 00:02:09 +0530

    WinDev Assignments

---
+++ 01-Win32/08-DLL/01-Linking/02-Explicit/Window.cpp
@@ -0,0 +1,105 @@
+//headers
+#include <windows.h>
+#include "Window.h"
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//entry point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    //variable declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("Explicit DLL");
+
+    //code
+    //initialize WNDCLASSEX
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+    //register above class
+    RegisterClassEx(&wndclass);
+
+    //create the window in memory
+    hwnd = CreateWindow(szAppName,
+        szAppName,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL, 
+        NULL,
+        hInstance,
+        NULL);
+    
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    //message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    //variable declarations
+    HMODULE hLib = NULL;
+    typedef int (*PFNMAKESQUARE)(int);
+    PFNMAKESQUARE pfnMakeSquare = NULL;
+    int num1, num2;
+    TCHAR str[255];
+
+    //code
+    switch(iMsg)
+    {
+        case WM_CREATE:
+            hLib = LoadLibrary(TEXT("MyMath.dll"));
+            if(hLib == NULL)
+            {
+                MessageBox(hwnd, TEXT("Failed to load library"), TEXT("Error"), MB_OK | MB_ICONERROR);
+                DestroyWindow(hwnd);
+            }
+
+            pfnMakeSquare = (PFNMAKESQUARE)GetProcAddress(hLib, "MakeSquare");
+            if(pfnMakeSquare == NULL)
+            {
+                MessageBox(hwnd, TEXT("Failed to get MakeSquare procedure"), TEXT("Error"), MB_OK | MB_ICONERROR);
+                DestroyWindow(hwnd);
+            }
+
+            num1 = 15;
+            num2 = pfnMakeSquare(num1);
+            wsprintf(str, TEXT("The Square of %d is %d."), num1, num2);
+            MessageBox(hwnd, str, TEXT("Square"), MB_OK);
+            FreeLibrary(hLib);
+            hLib = NULL;
+            break;
+        
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        
+        default:
+            break;
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
